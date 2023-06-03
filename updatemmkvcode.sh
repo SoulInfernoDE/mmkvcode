36dd4812eeb5fa94fe21f250fe7a56c3224c9778
Soul Inferno DE <SoulInfernoDE@users.noreply.github.com>
2023-06-03 19:54:06 +0200

    Update updatemmkvcode.sh
    updated file to reflect the active user home directory installation seen in var $HOME instead of only usable with the root user

--- updatemmkvcode.sh
+++ updatemmkvcode.sh
@@ -8,32 +8,32 @@ do
 	case "$optname" in
 		"w")
 			echo "Running from web"
-			$SCRIPTDIR/scripts/get-web.sh
+			$SCRIPTDIR/get-web.sh
 			;;
 		"p")
 			echo "Running From php"
-			$PHPBIN $SCRIPTDIR/scripts/get-php.php
+			$PHPBIN $SCRIPTDIR/get-php.php
 			;;
 		"?")
 			echo "Unknown Argument - defaulting to web"
-			$SCRIPTDIR/scripts/get-web.sh
+			$SCRIPTDIR/get-web.sh
 			;;
 	esac
 done
 if [ $OPTIND -eq 1 ]; then
 	echo "Argument set - defaulting to web"
-	$SCRIPTDIR/scripts/get-web.sh
+	$SCRIPTDIR/get-web.sh
 fi
 
 if [ -f "/tmp/tmpcode" ]; then
 	echo "Found /tmp/tmpcode"
-	if [ -f "/root/.MakeMKV/settings.conf" ]; then
-		echo "/root/.MakeMKV/settings.conf found - Updating app_key"
-		sed -ri 's|app_key = (.*)|app_key = "'`cat /tmp/tmpcode`'"|g' /root/.MakeMKV/settings.conf
+	if [ -f "/$HOME/.MakeMKV/settings.conf" ]; then
+		echo "/$HOME/.MakeMKV/settings.conf found - Updating app_key"
+		sed -ri 's|app_key = (.*)|app_key = "'`cat /tmp/tmpcode`'"|g' /$HOME/.MakeMKV/settings.conf
 	else
-		echo "/root/.MakeMKV/settings.conf not found - Creating"
-		mkdir -p /root/.MakeMKV/
-		echo "app_key = \"`cat /tmp/tmpcode`\"" > /root/.MakeMKV/settings.conf
+		echo "/$HOME/.MakeMKV/settings.conf not found - Creating"
+		mkdir -p /$HOME/.MakeMKV/
+		echo "app_key = \"`cat /tmp/tmpcode`\"" > /$HOME/.MakeMKV/settings.conf
 	fi		
 fi
 
